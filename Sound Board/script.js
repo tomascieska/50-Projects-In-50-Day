d86592162a71e42c23e423539996f93a6f6baf9f
tomascieska <tomas.cieska@gmail.com>
2023-12-04 21:16:42 +0000

    pr Sound Board

--- Sound Board/script.js
+++ Sound Board/script.js
@@ -6,9 +6,19 @@ sounds.forEach(sound => {
 
     btn.innerText = sound
     btn.addEventListener('click', () => {
+        stopSong()
         document.getElementById(sound).play()
     })
 
     document.getElementById('buttons').
     appendChild(btn)
 })
+
+function stopSong() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0
+    })
+}
