7390c6f7ea1f6f3a88bb9bf0ebf55575f9b3de2f
tomascieska <tomas.cieska@gmail.com>
2023-12-08 00:58:27 +0000

    pr Choice Picker

--- Choice Picker/script.js
+++ Choice Picker/script.js
@@ -29,7 +29,8 @@ function createTags(input){
 }
 
 function randomSelect() {
-    const time = 30
+    console.log(123)
+    const times = 30
 
     const interval = setInterval(() => {
         const randomTag = pickRandomTag()
@@ -40,13 +41,28 @@ function randomSelect() {
             unHighlightTag(randomTag)
         },100)
     }, 100)
+
+    setTimeout(() =>{
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
 }
 
 function pickRandomTag() {
     const tags = document.querySelectorAll('.tag')
-    return tags[Manth.floor(Math.random() * tags.length)]
+    return tags[Math.floor(Math.random() * tags.length)]
 }
 
 function highlightTag(tag) {
     tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
