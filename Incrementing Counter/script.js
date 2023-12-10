4005784d763698fd8c90bd4ee4d9b061d1ac5fda
tomascieska <tomas.cieska@gmail.com>
2023-12-10 18:59:58 +0000

    pr Incrementing Counter

--- Incrementing Counter/script.js
+++ Incrementing Counter/script.js
@@ -7,7 +7,15 @@ counters.forEach(counter => {
         const target = +counter.getAttribute('data-target')
         const c = +counter.innerText
 
-        const increment = target / 200
-        
+        const increment = target / 300
+
+        if(c < target){
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
     }
+
+    updateCounter()
 })
